b845cbc7e2b0e60351795f2ae74ea631fe5d257e
cdricn <cdricncdricn@gmail.com>
2024-10-17 01:41:40 +0800

    Fixed ProfilePage scaling clamps

--- src/webpages/ProfilePage.tsx
+++ src/webpages/ProfilePage.tsx
@@ -11,8 +11,8 @@ function ProfilePage() {
             <span>Birthday:</span> May 14th <br/>
             <span>Likes:</span> Games, peonies, potatoes, English <br/>
             <span>Dislikes:</span> Ghosts, worms, tomatoes <br/><br/>
-            Peo is a flower fairy living in Fairyland, above the <br/>
-            sky in Japan! She may be a shy and introverted <br/>
+            Peo is a flower fairy living in Fairyland, above the
+            sky in Japan! She may be a shy and introverted
             girl, but she loves to chat with everyone! <br/><br/>
 
             She is studying English hard to chat with everyone.
